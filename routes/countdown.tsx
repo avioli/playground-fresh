1453be4062a5587f593add4490c45786ec1c0774
Evo Stamatov <aviolito@gmail.com>
2022-07-01 11:53:55 +1000

    set the countdown date to 1st of Jan on the following year

--- routes/countdown.tsx
+++ routes/countdown.tsx
@@ -3,8 +3,7 @@ import { h } from "preact";
 import Countdown from "../islands/Countdown.tsx";
 
 export default function Page() {
-  const date = new Date();
-  date.setHours(date.getHours() + 1);
+  const date = new Date(`${new Date().getFullYear() + 1}-01-01T00:00:00.000+11:00`);
   return (
     <p>
       The big event is happening <Countdown target={date.toISOString()} />.
